5c30ee01c2c0d6d36b746a70de028802b83eb426
Mr. Pi <github@mr-pi.de>
2013-12-03 10:24:09 +0100

    basics

---
+++ server/src/wtfs_chat_app.erl
@@ -0,0 +1,70 @@
+%%%-------------------------------------------------------------------
+%%% @author Mr. Pi <mrpi@mr-pi.de>
+%%% @copyright 2013 Mr. Pi
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(wtfs_chat_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2,
+	 stop/1]).
+
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[1,2], and should start the processes of the
+%% application. If the application is structured according to the OTP
+%% design principles as a supervision tree, this means starting the
+%% top supervisor of the tree.
+%%
+%% @spec start(StartType, StartArgs) -> {ok, Pid} |
+%%                                      {ok, Pid, State} |
+%%                                      {error, Reason}
+%%      StartType = normal | {takeover, Node} | {failover, Node}
+%%      StartArgs = term()
+%% @end
+%%--------------------------------------------------------------------
+start(_StartType, _StartArgs) ->
+	application:ensure_started(httpStatusCodes),
+	application:ensure_started(ranch),
+	application:ensure_started(cowlib),
+	application:ensure_started(cowboy),
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			
+			{"/[...]", cowboy_static, {dir, application:get_env(wtfs_chat,root,"/var/www/")}}
+		]}
+	]),
+	{ok, _} = cowboy:start_http(http, 100, [{port, 40080}], [
+		{env, [{dispatch, Dispatch}]},
+		{onresponse, fun wtfs_chat_http_error:respond/4}
+	]),
+	wtfs_chat_sup:start_link().
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @spec stop(State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+stop(_State) ->
+	ok.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
---
+++ server/src/wtfs_chat_http_error.erl
@@ -0,0 +1,40 @@
+%%%-------------------------------------------------------------------
+%%% @author Mr. Pi <mrpi@mr-pi.de>
+%%% @copyright 2013 Mr. Pi
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(wtfs_chat_http_error).
+
+-export([respond/4]).
+
+respond(404, Headers, <<>>, Req) ->
+	{Path, Req2} = cowboy_req:path(Req),
+	Body = <<"404 Not Found: \"", Path/binary, "\"\n">>,
+	Headers2 = lists:keyreplace(<<"content-length">>, 1, Headers,
+			{<<"content-length">>, integer_to_list(byte_size(Body))}),
+	Headers3 = lists:keyreplace(<<"content-type">>, 1, Headers2,
+			{<<"content-type">>, "text/plain"}),
+	{ok, Req3} = cowboy_req:reply(404, Headers3, Body, Req2),
+	Req3;
+
+respond(Code, Headers, [], Req) when is_integer(Code), Code >= 400 ->
+	respond(Code, Headers, <<>>, Req);
+
+respond(Code, Headers, <<>>, Req) when is_integer(Code), Code >= 400 ->
+	Body = [
+			"HTTP Error: ",
+			integer_to_list(Code),
+			"(", httpStatusCodes:getDescription(Code), ")",
+			$\n
+			],
+	Headers2 = lists:keyreplace(<<"content-length">>, 1, Headers,
+			{<<"content-length">>, integer_to_list(iolist_size(Body))}),
+	Headers3 = lists:keyreplace(<<"content-type">>, 1, Headers2,
+			{<<"content-type">>, "text/plain"}),
+	{ok, Req2} = cowboy_req:reply(Code, Headers3, Body, Req),
+	Req2;
+
+respond(_Code, _Headers, _Body, Req) ->
+	Req.
