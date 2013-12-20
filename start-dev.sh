d5204a4b63f24746ccddd3b07a3e1647bb980b20
Mr. Pi <github@mr-pi.de>
2013-12-20 13:01:38 +0100

    start wtc correctly

--- start-dev.sh
+++ start-dev.sh
@@ -4,5 +4,5 @@ rebar compile
 erl -name "${APPNAME}_${RANDOM}@data.mrpi" -pa ebin deps/*/ebin \
 	-eval "application:start(lager)" \
 	-eval "lager:set_loglevel(lager_console_backend,info)." \
-	-eval "application:start($APPNAME)."
+	-eval "${APPNAME}:start()."
 echo -e "\e[0;33mreturn value: \e[1;31m$?\e[0m"
