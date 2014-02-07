dee2f99cc7269559426b044908fa5294e5a7e36b
Mr. Pi <github@mr-pi.de>
2014-02-07 23:28:01 +0100

    http(s) error manager

--- src/wtc_http_error.erl
+++ src/wtc_http_error.erl
@@ -9,32 +9,31 @@
 
 -export([respond/4]).
 
+-define(UPDATE(Key,Value,In), lists:keyreplace(Key, 1, In, {Key, Value})).
+
+respond(Code, Headers, [], Req) ->
+	respond(Code, Headers, <<>>, Req);
+
 respond(404, Headers, <<>>, Req) ->
 	{Path, Req2} = cowboy_req:path(Req),
 	Body = <<"404 Not Found: \"", Path/binary, "\"\n">>,
-	Headers2 = lists:keyreplace(<<"content-length">>, 1, Headers,
-			{<<"content-length">>, integer_to_list(byte_size(Body))}),
-	Headers3 = lists:keyreplace(<<"content-type">>, 1, Headers2,
-			{<<"content-type">>, "text/plain"}),
-	{ok, Req3} = cowboy_req:reply(404, Headers3, Body, Req2),
-	Req3;
-
-respond(Code, Headers, [], Req) when is_integer(Code), Code >= 400 ->
-	respond(Code, Headers, <<>>, Req);
+	Headers2 = ?UPDATE(<<"content-length">>, integer_to_list(byte_size(Body)), Headers),
+	Headers3 = ?UPDATE(<<"content-type">>, "text/plain", Headers2),
+	respond(404, Headers3, Body, Req2);
 
 respond(Code, Headers, <<>>, Req) when is_integer(Code), Code >= 400 ->
-	Body = [
-			"HTTP Error: ",
-			integer_to_list(Code),
-			"(", httpStatusCodes:getDescription(Code), ")",
-			$\n
-			],
+	Body = [ "HTTP Error: ",
+		 integer_to_list(Code),
+		 "(", httpStatusCodes:getDescription(Code), ")",
+		 $\n ],
 	Headers2 = lists:keyreplace(<<"content-length">>, 1, Headers,
 			{<<"content-length">>, integer_to_list(iolist_size(Body))}),
 	Headers3 = lists:keyreplace(<<"content-type">>, 1, Headers2,
 			{<<"content-type">>, "text/plain"}),
-	{ok, Req2} = cowboy_req:reply(Code, Headers3, Body, Req),
-	Req2;
+	respond(Code, Headers3, Body, Req);
 
-respond(_Code, _Headers, _Body, Req) ->
-	Req.
+respond(Code, Headers, Body, Req) ->
+	HeadersFin = lists:keyreplace(<<"server">>, 1, Headers, {<<"server">>, "pi"}),
+	{ok, ReqFin} = cowboy_req:reply(Code, HeadersFin, Body, Req),
+	lager:notice("test"),
+	ReqFin.
